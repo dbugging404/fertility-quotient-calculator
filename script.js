a6f0601b40fcfed59cccdb704ed18752eb895c18
couspor <73649710+couspor@users.noreply.github.com>
2021-03-04 13:50:00 +0530

    :tada: Initial Commit.

---
+++ script.js
@@ -0,0 +1,74 @@
+function computeQuotient() {
+
+    var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+    var age =    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var bmi  =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 4, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var afc =    [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var amh10 =  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    // var d2fsh =  [2, 3, 4, 5, 5, 4, 4, 3, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var ageInput = document.getElementById("ageInput").value;
+    var bmiInput = document.getElementById("bmiInput").value;
+    var afcInput = document.getElementById("afcInput").value;
+    var amhInput = Number(document.getElementById("amhInput").value) * 10;
+    function map1(ageInput) {
+        let index = values.findIndex(values => values >= ageInput);
+        return age[index]*3;
+    }
+    function map2(bmiInput) {
+        let index = values.findIndex(values => values >= bmiInput);
+        return bmi[index];
+    }
+    function map3(afcInput) {
+        let index = values.findIndex(values => values >= afcInput);
+        return afc[index]*3;
+    }
+    function map4(amhInput) {
+        let index = values.findIndex(values => values >= amhInput);
+        return amh10[index]*3;
+    }
+    var score = map1(ageInput) + map2(bmiInput) + map3(afcInput) + map4(amhInput)
+    console.log(score)
+    var percentage = score*2
+    console.log(percentage)
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("percentage").innerHTML = percentage;
+
+    if (score <= 18)  {
+        document.getElementById("result").innerHTML = "Poor Fertility";
+    } else if (score > 18 && score <= 27) {
+        document.getElementById("result").innerHTML = "Low Fertility";
+    } else if (score > 27 && score <= 34) {
+        document.getElementById("result").innerHTML = "Average Fertility";
+    } else if (score > 34 && score <= 42) {
+        document.getElementById("result").innerHTML = "Good Fertility";
+    } else if (score > 42 && score <= 50) {
+        document.getElementById("result").innerHTML = "Best Fertility";
+    } else if (score > 50) {
+        document.getElementById("result").innerHTML = "Poor Fertility";
+    } else {
+        document.getElementById("result").innerHTML = "Not Applicable";
+    }    
+}   
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
